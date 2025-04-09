b9fcdbf6f4129e3979023940df964c544461ae9b
gengyiyang <gengyiyang@aliyun.com>
2025-04-09 09:53:30 +0000

    [test] update

--- src/npt/arch/loongarch.rs
+++ src/npt/arch/loongarch.rs
@@ -1,53 +1,55 @@
 use page_table_entry::loongarch64::LA64PTE;
-use page_table_multiarch::{PageTable64, PagingMetaData};
+use page_table_multiarch::{PageTable64, loongarch64::LA64MetaData};
 use core::arch::asm;
+use page_table_multiarch::PagingMetaData;
 
 use crate::GuestPhysAddr;
 
 /// Metadata of LoongArch64 page tables.
-#[derive(Copy, Clone, Debug)]
-pub struct LA64MetaData;
+// #[derive(Copy, Clone, Debug)]
+// pub struct LA64MetaData;
+// impl LA64MetaData {
+//     /// PWCL(Page Walk Controller for Lower Half Address Space) CSR flags
+//     ///
+//     /// <https://loongson.github.io/LoongArch-Documentation/LoongArch-Vol1-EN.html#page-walk-controller-for-lower-half-address-space>
+//     ///
+//     /// | BitRange | Name      | Value |
+//     /// | ----     | ----      | ----  |
+//     /// | 4:0      | PTBase    |    12 |
+//     /// | 9:5      | PTWidth   |     9 |
+//     /// | 14:10    | Dir1Base  |    21 |
+//     /// | 19:15    | Dir1Width |     9 |
+//     /// | 24:20    | Dir2Base  |    30 |
+//     /// | 29:25    | Dir2Width |     9 |
+//     /// | 31:30    | PTEWidth  |     0 |
+//     pub const PWCL_VALUE: u32 = 12 | (9 << 5) | (21 << 10) | (9 << 15) | (30 << 20) | (9 << 25);
 
-impl LA64MetaData {
-    /// PWCL(Page Walk Controller for Lower Half Address Space) CSR flags
-    ///
-    /// <https://loongson.github.io/LoongArch-Documentation/LoongArch-Vol1-EN.html#page-walk-controller-for-lower-half-address-space>
-    ///
-    /// | BitRange | Name      | Value |
-    /// | ----     | ----      | ----  |
-    /// | 4:0      | PTBase    |    12 |
-    /// | 9:5      | PTWidth   |     9 |
-    /// | 14:10    | Dir1Base  |    21 |
-    /// | 19:15    | Dir1Width |     9 |
-    /// | 24:20    | Dir2Base  |    30 |
-    /// | 29:25    | Dir2Width |     9 |
-    /// | 31:30    | PTEWidth  |     0 |
-    pub const PWCL_VALUE: u32 = 12 | (9 << 5) | (21 << 10) | (9 << 15) | (30 << 20) | (9 << 25);
+//     /// PWCH(Page Walk Controller for Higher Half Address Space) CSR flags
+//     ///
+//     /// <https://loongson.github.io/LoongArch-Documentation/LoongArch-Vol1-EN.html#page-walk-controller-for-higher-half-address-space>
+//     ///
+//     /// | BitRange | Name                            | Value |
+//     /// | ----     | ----                            | ----  |
+//     /// | 5:0      | Dir3Base                        |    39 |
+//     /// | 11:6     | Dir3Width                       |     9 |
+//     /// | 17:12    | Dir4Base                        |     0 |
+//     /// | 23:18    | Dir4Width                       |     0 |
+//     /// | 24       | 0                               |     0 |
+//     /// | 24       | HPTW_En(CPUCFG.2.HPTW(bit24)=1) |     0 |
+//     /// | 31:25    | 0                               |     0 |
+//     pub const PWCH_VALUE: u32 = 39 | (9 << 6);
+// }
+pub struct MyLA64MetaData(LA64MetaData);
 
-    /// PWCH(Page Walk Controller for Higher Half Address Space) CSR flags
-    ///
-    /// <https://loongson.github.io/LoongArch-Documentation/LoongArch-Vol1-EN.html#page-walk-controller-for-higher-half-address-space>
-    ///
-    /// | BitRange | Name                            | Value |
-    /// | ----     | ----                            | ----  |
-    /// | 5:0      | Dir3Base                        |    39 |
-    /// | 11:6     | Dir3Width                       |     9 |
-    /// | 17:12    | Dir4Base                        |     0 |
-    /// | 23:18    | Dir4Width                       |     0 |
-    /// | 24       | 0                               |     0 |
-    /// | 24       | HPTW_En(CPUCFG.2.HPTW(bit24)=1) |     0 |
-    /// | 31:25    | 0                               |     0 |
-    pub const PWCH_VALUE: u32 = 39 | (9 << 6);
-}
-
-impl PagingMetaData for LA64MetaData {
+impl PagingMetaData for MyLA64MetaData {
     const LEVELS: usize = 4;
     const PA_MAX_BITS: usize = 48;
     const VA_MAX_BITS: usize = 48;
-    type VirtAddr = memory_addr::VirtAddr;
-
+    // type VirtAddr = memory_addr::VirtAddr;
+    type VirtAddr = GuestPhysAddr;
+    
     #[inline]
-    fn flush_tlb(vaddr: Option<memory_addr::VirtAddr>) {
+    fn flush_tlb(vaddr: Option<Self::VirtAddr>) {
         unsafe {
             if let Some(vaddr) = vaddr {
                 // <https://loongson.github.io/LoongArch-Documentation/LoongArch-Vol1-EN.html#_dbar>
@@ -82,11 +84,12 @@ impl PagingMetaData for LA64MetaData {
 /// 4 levels:
 ///
 /// using page table dir3, dir2, dir1 and pt, ignore dir4
-pub type LA64PageTable<I> = PageTable64<LA64MetaData, LA64PTE, I>;
+// pub type LA64PageTable<I> = PageTable64<LA64MetaData, LA64PTE, I>;
 
 // impl PagingHandler for addr::GuestPhysAddr {
 //     // Implement required methods here
 // }
 
-pub type NestedPageTable<H> = PageTable64<LA64PageTable<GuestPhysAddr>, LA64PTE, H>;
+// pub type NestedPageTable<H> = PageTable64<LA64MetaData<GuestPhysAddr>, LA64PTE, H>;
 
+pub type NestedPageTable<H> = PageTable64<MyLA64MetaData, LA64PTE, H>;
